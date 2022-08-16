92b9a88272765ee56b993736091e9da8172041e9
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-08-16 12:34:09 -0400

    Delete node_modules directory

--- node_modules/get-website-favicon/test.js
+++
@@ -1,7 +0,0 @@
-const getFavicons = require('./')
-const url = process.argv[2] || 'http://github.com'
-getFavicons(url).then(result=>{
-    console.log(result)
-}).catch(error=>{
-    console.log(error)
-})
--- node_modules/get-website-favicon/test/index.js
+++
@@ -1,7 +0,0 @@
-const getFavicons = require('../')
-const expect = require('chai').expect;
-
-describe('', ()=>{
-    it('', async function(){
-    })
-})
