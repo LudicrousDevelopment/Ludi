86103228bc359b3abd86f92b60129f92f7253f28
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-08-16 12:35:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,22 +7,10 @@
     primaryPrxxy: 'ultraviolet',
     googleBlock: true,
     cors: true,
-    uk: false,
-    an: false,
-    rammerhead:	false, 
-    rammerheadURL: 'r.911911911.info',
     emulator: true,
     config: {
       port: 2000,
       gamePort: 3000,
-    },
-    links: [
-      "https://fractiontools.com",
-      "https://pioneersites.org",
-      "https://dailylights.org", 
-      "https://calculatehelp.org",
-      "https://erraticphysics.com",
-      "https://packcheap.net",
-    ]
+    }
   }));
 })();
